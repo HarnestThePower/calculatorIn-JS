80c8c14c6dc39db2b3a90981f8c6c16696e28c03
Anu <gdstar37@hotmail.com>
2017-05-13 14:31:33 +0545

    Add files via upload

---
+++ scriptcalc.js
@@ -0,0 +1,11 @@
+function display(x){
+	form.txt.value=form.txt.value+x;
+}
+
+function ce(){
+	form.txt.value="";
+}
+
+function c(){
+	form.txt.value = form.txt.value.slice(0,form.txt.value.length-1);
+}
